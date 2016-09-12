32e896ca8fb1d63dc4b1714169c4baceec3a73a5
Eduardo Nunes <eduardonunes2525@gmail.com>
2016-09-12 17:48:50 -0300

    Add multiplicação

--- src/calc/Calculadora.java
+++ src/calc/Calculadora.java
@@ -2,4 +2,14 @@ package calc;
 
 public class Calculadora {
 
+
+
+
+private double numero1;
+private double numero2;
+
+
+  public double multiplica(double numero1, double numero2){
+    return numero1*numero2;
+  } 
 }
