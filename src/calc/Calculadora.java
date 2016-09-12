90afc9c9377a4ae6054e97bc8c7bed56ce4ae392
Jhonatan Alves <jhon_fla@hotmail.com>
2016-09-12 17:49:46 -0300

    Creating subtraction method

--- src/calc/Calculadora.java
+++ src/calc/Calculadora.java
@@ -1,5 +1,12 @@
 package calc;
 
 public class Calculadora {
-
+    
+    public Double subtraction(Double firstNumber, Double secondNumber){
+        Double result;
+        
+        result = firstNumber - secondNumber;
+        
+        return result;
+    }
 }
